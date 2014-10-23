591d3f0ac822730eae1829aea7cc9c7428d39741
BigFun <fzy0201k@gmail.com>
2014-10-23 20:45:48 +0800

    modify codekata_1

--- player/BigFun/Add/test.cpp
+++ player/BigFun/Add/test.cpp
@@ -4,20 +4,40 @@ using namespace std;
 
 extern string Add(const string &str1, const string &str2);
 
+void No_Carrybit_Test()
+{
+	assert("3" == Add("1","2"));
+}
+
+void Single_Carrybit_Test()
+{
+	assert("10" == Add("1","9"));
+	assert("11" == Add("2","9"));
+}
+
+void Multi_Carrybit_Test()
+{
+	assert("100" == Add("1","99"));
+	assert("1000" == Add("1","999"));
+}
+
+void Complex_Operation_Test()
+{
+	assert("101" == Add("2","99"));
+	assert("198" == Add("99","99"));
+	assert("12444" == Add("12345","99"));
+	assert("123456789123456789123457011" == Add("123456789123456789123456789", "222"));
+}
+
 void main()
 {
-	assert(Add("1","2") == "3");
+	No_Carrybit_Test();
 
-	assert(Add("1","9") == "10");
-	assert(Add("2","9") == "11");
+	Single_Carrybit_Test();
 
-	assert(Add("1","99") == "100");
-	assert(Add("1","999") == "1000");
-	assert(Add("1","9999") == "10000");
+	Multi_Carrybit_Test();
 
-	assert(Add("2","99") == "101");
-	assert(Add("12345","99") == "12444");
-	assert(Add("123456789123456789123456789", "222") == "123456789123456789123457011");
+	Complex_Operation_Test();
 
 	getchar();
 }
